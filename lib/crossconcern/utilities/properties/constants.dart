8168b50edf285c5983c4803dea8e045141599802
Alessandro De Nard <a.denard@hastega.it>
2022-03-08 10:19:29 +0100

    IZIDV21-39 Added App Icons to Android and iOS

--- lib/crossconcern/utilities/properties/constants.dart
+++ lib/crossconcern/utilities/properties/constants.dart
@@ -4,11 +4,5 @@ const String appName = 'Homsai';
 // height of the 'Homsai' header
 const double homsaiHeaderHeight = 64;
 
-// The font size delta for headline4 font.
-const double desktopDisplay1FontDelta = 16;
-
-// Sentinel value for the system text scale factor option.
-const double systemTextScaleFactorOption = -1;
-
 // The splash page animation duration.
 const splashPageAnimationDurationInMilliseconds = 300;
